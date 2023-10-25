bc649da200413ca36ba392f2bce7c7b6ae2a9a12
ihor-doroschenko <igordoroshenko@outlook.com>
2023-10-25 19:51:21 +0200

    rebase 1

--- src/App.js
+++ src/App.js
@@ -1,64 +1,130 @@
-import Typography from "@material-ui/core/Typography";
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList";
-import fetchTodos from "./service/api";
+import { Box, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import classnames from 'classnames';
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Buttons from './components/Buttons';
+import MainForm from './components/MainForm';
 
-const LOCAL_STORAGE_KEY = "todo-list";
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexWrap: 'wrap',
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#f4f4f4',
+    display: 'flex',
+    justifyContent: 'center',
+    '& > *': {
+      padding: '20px',
+      display: 'flex',
+      flexDirection: 'column',
+    },
+  },
+}));
+
+const LOCAL_STORAGE_KEY = 'data';
 
 function App() {
-    const [todos, setTodos] = useState([]);
+  const classes = useStyles();
+  const [formData, setFormData] = useState({
+    streckennummer: '',
+    photo: null,
+  });
 
-    useEffect(() => {
-        fetchTodos()
-            .then(result => {
-                setTodos(result);
-            });
+  const [submissions, setSubmissions] = useState([]); // Store all submissions
 
-    }, []);
-
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
-    }, [todos]);
-
-    function addTodo(todo) {
-        setTodos([todo, ...todos]);
+  useEffect(() => {
+    // Load data from local storage
+    const storedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+      setSubmissions(parsedData);
     }
+  }, []);
 
-    function toggleComplete(id) {
-        if (todos) {
-            setTodos(
-                todos.map(todo => {
-                    if (todo.id === id) {
-                        return {
-                            ...todo,
-                            completed: !todo.completed
-                        };
-                    }
-                    return todo;
-                })
-            );
-        }
+  useEffect(() => {
+    // Update local storage whenever the submissions array changes
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(submissions));
+  }, [submissions]);
+
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handlePhotoChange = e => {
+    const photo = e.target.files[0];
+    setFormData({
+      ...formData,
+      photo,
+    });
+  };
+
+  const handleSubmit = () => {
+    const reader = new FileReader();
+    reader.onload = event => {
+      const base64Photo = event.target.result;
+      // Append new submission to the array
+      setSubmissions([...submissions, { ...formData, photo: base64Photo }]);
+      // Save to local storage each time the user submits
+      localStorage.setItem(
+        LOCAL_STORAGE_KEY,
+        JSON.stringify([...submissions, { ...formData, photo: base64Photo }])
+      );
+    };
+    reader.readAsDataURL(formData.photo);
+  };
+
+  const downloadPhoto = React.useCallback(() => {
+    for (let el of submissions) {
+      const link = document.createElement('a');
+      link.href = el.photo;
+      link.download = `${el.streckennummer}.png`;
+      link.click();
     }
+  }, [submissions]);
 
-    function removeTodo(id) {
-        setTodos(todos.filter(todo => todo.id !== id));
-    }
+  const downloadCSV = () => {
+    // Generate CSV content
+    const csvContent =
+      'data:text/csv;charset=utf-8,' +
+      'Streckennummer,Photo\n' +
+      submissions.map(entry => `"${entry.streckennummer}",${entry.photo}\n`).join('');
 
-    return (
-        <div className="App">
-            <Typography style={{ padding: 16 }} variant="h1">
-                React Todo
-            </Typography>
-            <TodoForm addTodo={addTodo} />
-            <TodoList
-                todos={todos}
-                removeTodo={removeTodo}
-                toggleComplete={toggleComplete}
-            />
-        </div>
-    );
+    // Create and trigger a download link
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement('a');
+    link.setAttribute('href', encodedUri);
+    link.setAttribute('download', 'data.csv');
+    document.body.appendChild(link);
+    link.click();
+  };
+
+  return (
+    <div className={classnames(classes.root, 'appWrapper')}>
+      <Paper elevation={3} classes={classes.paper}>
+        <Box>
+          <Typography style={{ padding: 16 }} variant='h4'>
+            Außendienst App
+          </Typography>
+        </Box>
+        <MainForm
+          formData={formData}
+          handleInputChange={handleInputChange}
+          handlePhotoChange={handlePhotoChange}
+        />
+        <Buttons
+          handleSubmit={handleSubmit}
+          downloadCSV={downloadCSV}
+          downloadPhoto={downloadPhoto}
+        />
+      </Paper>
+    </div>
+  );
 }
 
 export default App;
